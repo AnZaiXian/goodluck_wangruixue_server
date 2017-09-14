88fe249ea56fe797348470b0bdf18a1542b8a8c1
wangruixue <3304682983@qq.com>
2017-09-14 13:56:37 +0800

    goodluck_wangruixue_server

---
+++ src/main/java/com/bw/wangruixue/GoodluckWangruixueServerApplication.java
@@ -0,0 +1,12 @@
+package com.bw.wangruixue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodluckWangruixueServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodluckWangruixueServerApplication.class, args);
+	}
+}
